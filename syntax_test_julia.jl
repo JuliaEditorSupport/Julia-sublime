10ec44ebf45465b381dd0e742e3c9bb4bf964149
Viktor Qvarfordt <viktor.qvarfordt@gmail.com>
2016-08-11 19:58:41 +0200

    Rewrite to new sublime-syntax format and att tests

---
+++ syntax_test_julia.jl
@@ -0,0 +1,494 @@
+# SYNTAX TEST "Packages/Julia-sublime/Julia.sublime-syntax"
+
+
+##
+## NUMBERS
+##
+
+  0b101
+# ^^^^^ constant.numeric
+  0o7
+# ^^^ constant.numeric
+  0xa3
+# ^^^^ constant.numeric
+  e+123
+# ^^^^^ constant.numeric
+  12e123
+# ^^^^^^ constant.numeric
+  1.32e+123
+# ^^^^^^^^^ constant.numeric
+  e-123
+# ^^^^^ constant.numeric
+  11
+# ^^ constant.numeric
+  .11
+# ^^^ constant.numeric
+  11.
+# ^^^ constant.numeric
+  11.11
+# ^^^^^ constant.numeric
+  2.a
+# ^^ constant.numeric
+#   ^ variable.other
+
+##
+## CONSTANTS
+##
+
+  true
+# ^^^^ constant.language
+  false
+# ^^^^^ constant.language
+  nothing
+# ^^^^^^^ constant.language
+  NaN
+# ^^^ constant.language
+  Inf
+# ^^^ constant.language
+
+
+##
+## FUNCITON CALLS
+##
+
+# (issue 9, 16)
+  !β!(x)
+# ^ keyword.operator
+#  ^^ variable.function
+#     ^ variable.other
+  (x,f(x))
+#  ^ variable.other
+#    ^ variable.function
+#      ^ variable.other
+  √(2.3)
+# ^ variable.function
+#   ^^^ constant.numeric
+
+
+##
+## UNICODE WORD BOUDARIES
+##
+
+# Numbers in variable names (issue 18)
+  β1 = 5
+# ^^ variable.other
+  β3(x)
+# ^^ variable.function
+  β2(x) = x
+# ^^ entity.name.function
+  ∇1 = 5
+# ^^ variable.other
+  ∇3(x)
+# ^^ variable.function
+  ∇2(x) = x
+# ^^ entity.name.function
+
+# (issue 11)
+  a::B{C}=c()
+#  ^^ keyword.operator
+#    ^^^^ support.type
+#        ^ keyword.operator
+#         ^ variable.function
+
+
+##
+## SYMBOLS
+##
+
+# (issue 3)
+  :1
+# ^ keyword.operator
+#  ^ constant.other.symbol
+  :β
+# ^ keyword.operator
+#  ^ constant.other.symbol
+  :+
+# ^ keyword.operator
+#  ^ constant.other.symbol
+  :∘
+# ^ keyword.operator
+#  ^ constant.other.symbol
+  :∘^a
+# ^ keyword.operator
+#  ^ constant.other.symbol
+#   ^ keyword.operator
+#    ^ variable.other
+  :a!
+# ^ keyword.operator
+#  ^^ constant.other.symbol
+  :(:)
+# ^ keyword.operator
+#  ^ source.julia
+#    ^ source.julia
+  :(a)
+# ^ keyword.operator
+#  ^ source.julia
+#   ^ variable.other
+#      ^ source.julia
+
+
+##
+## RANGES
+##
+
+# (issue 14)
+  a:b
+# ^ variable.other
+#  ^ keyword.operator
+#   ^ variable.other
+  1.:a
+# ^^ constant.numeric
+#   ^ keyword.operator
+#    ^ variable.other
+  a:2.
+# ^ variable.other
+#  ^ keyword.operator
+#   ^^ constant.numeric
+  23.:31.
+# ^^^ constant.numeric
+#    ^ keyword.operator
+#     ^^^ constant.numeric
+  β:f()
+# ^ variable.other
+#  ^ keyword.operator
+#   ^ variable.function
+  f():1
+# ^ variable.function
+#    ^ keyword.operator
+#     ^ constant.numeric
+
+
+##
+## TYPES
+##
+
+  f(x::Foo, ::Bar{1, 2})
+# ^ variable.function
+#   ^ variable.other
+#    ^^ keyword.operator
+#      ^^^ support.type
+#           ^^ keyword.operator
+#             ^^^^^^^^^ support.type
+# Nested types
+  x::A{B,C{D,E},F{G,H{I,J{K,L{M}},N}}} closed
+#  ^^ keyword.operator
+#    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ support.type
+# (issue 19)
+  myvar::MyModule.MyType
+# ^^^^^ variable.other
+#      ^^ keyword.operator
+#        ^^^^^^^^^^^^^^^ support.type
+
+# Modules (issue 19)
+  MyModule.myfunc(x)
+# ^^^^^^^^ variable.other
+#         ^ keyword.operator
+#          ^^^^^^ variable.function
+#                 ^ variable.other
+
+# (issue 11)
+  aa::AA{BB} = fun{CC{DD}}()
+# ^^ variable.other
+#   ^^ keyword.operator
+#     ^^^^^^ support.type
+#            ^ keyword.operator
+#              ^^^ variable.function
+#                 ^^^^^^^^ support.type
+  aa::AA{BB}=fun{CC{DD}}()
+# ^^ variable.other
+#   ^^ keyword.operator
+#     ^^^^^^ support.type
+#           ^ keyword.operator
+#            ^^^ variable.function
+#               ^^^^^^^^ support.type
+
+# (issue 17)
+# All things being defined are green, types as well
+  type Foo{T<:Real} end
+# ^^^^ keyword.other
+#      ^^^^^^^^^^^^ entity.name.function
+  type Foo{T}<:Bar{T} end
+# ^^^^ keyword.other
+#      ^^^^^^ entity.name.function
+#            ^^ keyword.operator
+#              ^^^^^^ support.type
+  immutable Foo{T<:Real} end
+# ^^^^^^^^^ keyword.other
+#           ^^^^^^^^^^^^ entity.name.function
+  typealias Foo{T} Bar{T}
+# ^^^^^^^^^ keyword.other
+#           ^^^^^^ entity.name.function
+#                  ^^^^^^ support.type
+  bitstype 32 Foo{T}<:Bar{T}
+# ^^^^^^^^ keyword.other
+#          ^^ constant.numeric
+#             ^^^^^^ entity.name.function
+#                   ^^ keyword.operator
+#                     ^^^^^^ support.type
+
+
+##
+## FUNCTION DEFINITIONS
+##
+
+  function Module.foo!{T<:Real}(xx::Aa{Bb}, β::Aa{Bb}=1.::Aa{Bb}, c) end
+# ^^^^^^^^ keyword.other
+#          ^^^^^^ variable.other
+#                ^ keyword.operator
+#                 ^^^^ entity.name.function
+#                     ^^^^^^^^^ support.type
+#                               ^^ variable.parameter
+#                                 ^^ keyword.operator
+#                                   ^^^^^^ support.type
+#                                           ^ variable.parameter
+#                                            ^^ keyword.operator
+#                                              ^^^^^^ support.type
+#                                                    ^ keyword.operator
+#                                                     ^^ constant.numeric
+#                                                       ^^ keyword.operator
+#                                                         ^^^^^^ support.type
+#                                                                 ^ variable.parameter
+#                                                                    ^^^ keyword.other
+
+# Infix operator declaration
+  function ∘{T<:Real}(xx::Aa{Bb}, β::Aa{Bb}=1.::Aa{Bb}, c) end
+# ^^^^^^^^ keyword.other
+#          ^ entity.name.function
+#           ^^^^^^^^^ support.type
+#                     ^^ variable.parameter
+#                       ^^ keyword.operator
+#                         ^^^^^^ support.type
+#                                 ^ variable.parameter
+#                                  ^^ keyword.operator
+#                                    ^^^^^^ support.type
+#                                          ^ keyword.operator
+#                                           ^^ constant.numeric
+#                                             ^^ keyword.operator
+#                                               ^^^^^^ support.type
+#                                                       ^ variable.parameter
+#                                                          ^^^ keyword.other
+
+# Infix operator declaration in module
+  function Module.∘{T<:Real}(xx::Aa{Bb}, β::Aa{Bb}=1.::Aa{Bb}, c) end
+# ^^^^^^^^ keyword.other
+#          ^^^^^^ variable.other
+#                ^ keyword.operator
+#                 ^ entity.name.function
+#                  ^^^^^^^^^ support.type
+#                            ^^ variable.parameter
+#                              ^^ keyword.operator
+#                                ^^^^^^ support.type
+#                                        ^ variable.parameter
+#                                         ^^ keyword.operator
+#                                           ^^^^^^ support.type
+#                                                 ^ keyword.operator
+#                                                  ^^ constant.numeric
+#                                                    ^^ keyword.operator
+#                                                      ^^^^^^ support..type
+#                                                              ^ variable.parameter
+#                                                                 ^^^ keyword.other
+
+# "Mathematical" function declaration (issue 19)
+  Module.foo!{T<:Real}(xx::Aa{Bb}, β::Aa{Bb}=1.::Aa{Bb}, c) = kron(a, b)
+# ^^^^^^ variable.other
+#       ^ keyword.operator
+#        ^^^^ entity.name.function
+#            ^^^^^^^^^ support.type
+#                      ^^ variable.parameter
+#                        ^^ keyword.operator
+#                          ^^^^^^ support.type
+#                                  ^ variable.parameter
+#                                   ^^ keyword.operator
+#                                     ^^^^^^ support.type
+#                                           ^ keyword.operator
+#                                            ^^ constant.numeric
+#                                              ^^ keyword.operator
+#                                                ^^^^^^ support.type
+#                                                        ^ variable.parameter
+#                                                           ^ keyword.operator
+
+# "Mathematical" infix operator declaration
+  ∘{T<:Real}(xx::Aa{Bb}, β::Aa{Bb}=1.::Aa{Bb}, c) = ...
+# ^ entity.name.function
+#  ^^^^^^^^^ support.type
+#            ^^ variable.parameter
+#              ^^ keyword.operator
+#                ^^^^^^ support.type
+#                        ^ variable.parameter
+#                         ^^ keyword.operator
+#                           ^^^^^^ support.type
+#                                 ^ keyword.operator
+#                                  ^^ constant.numeric
+#                                    ^^ keyword.operator
+#                                      ^^^^^^ support.type
+#                                              ^ variable.parameter
+#                                                 ^ keyword.operator
+
+# "Mathematical" infix operator declaration in module
+  Module.∘{T<:Real}(xx::Aa{Bb}, β::Aa{Bb}=1.::Aa{Bb}, c) = ...
+# ^^^^^^ variable.other
+#       ^ keyword.operator
+#        ^ entity.name.function
+#         ^^^^^^^^^  support.type
+#                   ^^ variable.parameter
+#                     ^^ keyword.operator
+#                       ^^^^^^  support.type
+#                               ^ variable.parameter
+#                                ^^ keyword.operator
+#                                  ^^^^^^  support.type
+#                                        ^ keyword.operator
+#                                         ^^ constant.numeric
+#                                           ^^ keyword.operator
+#                                             ^^^^^^ support.type
+#                                                     ^ variable.parameter
+#                                                        ^ keyword.operator
+
+# Splats
+  function f(x..., x::Foo{T}...) end
+# ^^^^^^^^ keyword.other
+#          ^ entity.name.function
+#            ^ variable.parameter
+#             ^^^ keyword.operator
+#                  ^ variable.parameter
+#                   ^^ keyword.operator
+#                     ^^^^^^ support.type
+#                           ^^^ keyword.operator
+#                                ^^^ keyword.other
+
+# Parenthesized function name
+  function (+)(a) end
+# ^^^^^^^^ keyword.other
+#           ^ entity.name.function
+#              ^ variable.parameter
+#                 ^^^ keyword.other
+
+
+##
+## MACRO
+##
+
+  macro foo(a) end
+# ^^^^^ keyword.other
+#       ^^^ entity.name.function
+#           ^ variable.parameter
+  @foo(a)
+# ^^^^ variable.macro variable.parameter
+#      ^ variable.other
+
+
+##
+## TRANSPOSES
+##
+
+# (issue 1, 15)
+  a'
+# ^ variable.other
+#  ^ keyword.operator
+  β.''
+# ^ variable.other
+#  ^^^ keyword.operator
+  f()'
+# ^ variable.function
+#    ^ keyword.operator
+ (1.)'
+# ^^ constant.numeric
+#    ^ keyword.operator
+ [1,2]'
+# ^ constant.numeric
+#   ^ constant.numeric
+#     ^ keyword.operator
+
+
+##
+## INFIX OPERATORS
+##
+
+# (issue 6, 8, 10)
+  2.⊗a
+# ^^ constant.numeric
+#   ^ keyword.operator
+#    ^ variable.other
+  a⊕β()
+# ^ variable.other
+#  ^ keyword.operator
+#   ^ variable.function
+  1.23≤23.
+# ^^^^ constant.numeric
+#     ^ keyword.operator
+#      ^^^ constant.numeric
+
+
+##
+## COMMENTS
+##
+
+# (issue 13)
+  #fun()
+# ^^^^^^ comment.line
+# Nested block comments
+  #=#==#hej=#
+# ^^^^^^^^^^^ comment.block
+
+
+##
+## STIRNGS
+##
+
+"hello" closed
+# <- string.quoted.double
+# ^^^^^ string.quoted.double
+#         ^ variable.other
+'hello' closed
+# <- string.quoted.single
+# ^^^^^ string.quoted.single
+#       ^ variable.other
+  "hel\lo\"there"
+# ^^^^ string.quoted.double
+#     ^^ constant.character.escape
+#       ^ string.quoted.double
+#        ^^ constant.character.escape
+#          ^^^^^^ string.quoted.double
+# (issue 5)
+  r"\\\\" closed
+# ^^^^^^^ string.quoted.other
+#         ^ variable.other
+  r"raw\str\"ooh\\ok" closed
+# ^^^^^^^^^ string.quoted.other
+#          ^^ constant.character.escape
+#            ^^^ string.quoted.other
+#               ^^ constant.character.escape
+#                 ^^^ string.quoted.other
+#                     ^ variable.other
+  whatever"yaah" closed
+# ^^^^^^^^^^^^^^ string.quoted.other
+#                ^ variable.other
+  b"DATA\xffoho\u2200aha" closed
+# ^^^^^^ string.quoted.other
+#       ^^^^ constant.character.escape
+#           ^^^ string.quoted.other
+#              ^^^^^^ constant.character.escape
+#                    ^^^^ string.quoted.other
+#                         ^ variable.other
+
+
+##
+## STRING INTERPOLATION
+##
+
+a = "foo $∇2() bar" closed
+#   ^^^^^ string.quoted.double
+#        ^ keyword.operator
+#         ^^ variable.other
+#           ^^^^^^^ string.quoted.double
+#                   ^ variable.other
+a = "foo$(a+f(a, g())+b)(a)bar" closed
+#   ^^^^ string.quoted.double
+#       ^ keyword.operator
+#         ^ variable.other
+#          ^ keyword.operator
+#           ^ variable.function
+#             ^ variable.other
+#                ^ variable.function
+#                     ^ variable.other
+#                       ^^^^^^^ string.quoted.double
+#                               ^ variable.other
