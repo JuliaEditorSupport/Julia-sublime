65afbe7ca7268f9ac5661985b41865255652de19
Randy Lai <randy.cs.lai@gmail.com>
2016-10-04 13:08:12 -0400

    improve unicode completion

--- unicode.py
+++ unicode.py
@@ -30,11 +30,11 @@ def julia_unicode_has_prefix(view, check_match=False):
     if not check_match:
         return True
 
+    count = 0
     for s in symbols:
-        if c == s[0]:
-            return True
-
-    return False
+        if s[0].startswith(c):
+            count = count + 1
+    return count == 1
 
 
 class JuliaUnicodeListener(sublime_plugin.EventListener):
@@ -45,18 +45,7 @@ class JuliaUnicodeListener(sublime_plugin.EventListener):
         if view.score_selector(locations[0], "source.julia") == 0:
             return
 
-        ploc = locations[0]-len(prefix)
-        if view.substr(sublime.Region(ploc-1, ploc)) == "\\":
-            prefix = view.substr(sublime.Region(ploc-1, locations[0]))
-        elif view.substr(sublime.Region(ploc-2, ploc)) == "\\^":
-            prefix = view.substr(sublime.Region(ploc-1, locations[0]))
-        elif view.substr(sublime.Region(ploc-2, ploc)) == "\\:":
-            prefix = view.substr(sublime.Region(ploc-2, locations[0]))
-        elif view.substr(sublime.Region(ploc-1, ploc)) == ":":
-            pt = view.word(ploc-2).begin()
-            prefix = view.substr(sublime.Region(pt-2, locations[0]))
-        else:
-            return None
+        prefix = get_command(view)
         if not prefix:
             return None
         return [(s[0] + "\t" + s[1], s[1]) for s in symbols if prefix in s[0]]
@@ -76,6 +65,16 @@ class JuliaUnicodeInsertBestCompletion(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
         view.run_command("insert_best_completion",  {"default": "\t", "exact": False})
+        for sel in view.sel():
+            pt = sel.begin()
+            if view.substr(sublime.Region(pt-3, pt-1)) == "\\:":
+                view.replace(edit, sublime.Region(pt-3, pt-1), "")
+
+
+class JuliaUnicodeAutoComplete(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view = self.view
+        view.run_command("auto_complete")
         for sel in view.sel():
             pt = sel.begin()
             if view.substr(sublime.Region(pt-3, pt-1)) == "\\:":
