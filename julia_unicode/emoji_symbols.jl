73a881fdc5876f599c28598366de1fcf8f235dc3
Randy Lai <randy.cs.lai@gmail.com>
2019-12-10 12:11:22 -0800

    update symbols

--- julia_unicode/emoji_symbols.jl
+++ julia_unicode/emoji_symbols.jl
@@ -2,9 +2,10 @@ include(joinpath(Sys.BINDIR, "..", "share", "julia", "stdlib", "v$(VERSION.major
 
 📁 = open("emoji_symbols.py", "w")
 
+println(📁, "from __future__ import unicode_literals\n\n")
 println(📁, "emoji_symbols = [")
 for (📞, (🔑, 🍺)) in enumerate(emoji_symbols)
-    print(📁, "(\"", escape_string(🔑), "\", u\"",  🍺, "\")")
+    print(📁, "  (\"", escape_string(🔑), "\", u\"",  🍺, "\")")
     📞 < length(emoji_symbols) && print(📁, ",")
     println(📁, "")
 end
--- julia_unicode/latex_symbols.jl
+++ julia_unicode/latex_symbols.jl
@@ -2,9 +2,10 @@ include(joinpath(Sys.BINDIR, "..", "share", "julia", "stdlib", "v$(VERSION.major
 
 φ = open("latex_symbols.py", "w")
 
+println(φ, "from __future__ import unicode_literals\n\n")
 println(φ, "latex_symbols = [")
 for (ω, (α, β)) in enumerate(latex_symbols)
-    print(φ, "(\"", escape_string(α), "\", u\"",  β, "\")")
+    print(φ, "  (\"", escape_string(α), "\", u\"",  β, "\")")
     ω < length(latex_symbols) && print(φ, ",")
     println(φ, "")
 end
