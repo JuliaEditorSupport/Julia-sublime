182a8263ad2604860e8313f6f6604d603227346d
Randy Lai <randy.cs.lai@gmail.com>
2020-04-09 22:14:43 -0700

    anotation is back

--- unicode.py
+++ unicode.py
@@ -30,7 +30,7 @@ def normalize_completion(symbols):
                 trigger=s[0],
                 completion=s[1],
                 annotation=s[1],
-                kind=(sublime.KIND_ID_AMBIGUOUS, s[1] if len(s[1]) == 1 else " ", "unicode"))
+                kind=sublime.KIND_AMBIGUOUS)
                 for s in symbols),
             flags=sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
 
